f3779903050152c5c20d2c58b71f80351a1d5c11
ErinAtluri <125645174+ErinAtluri@users.noreply.github.com>
2023-06-01 21:45:58 -0700

    Pilgrim badge demo
    finished demo for pilgrim badge interaction.

---
+++ Assets/Scripts/BadgeEventSystem.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BadgeEventSystem : MonoBehaviour
+{
+
+    public GameObject WrongD1;
+    public GameObject WrongD2;
+    public GameObject RightD;
+
+    [SerializeField] Material triggerMat;
+    [SerializeField] private GameObject page;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        string name = page.GetComponent<MeshRenderer>().material.name;
+        if ((name == triggerMat.name + " (Instance)"))
+        {
+            if (col.gameObject.CompareTag("Badge 3 R"))
+            {
+                RightD.SetActive(true);
+            }
+
+            if (col.gameObject.CompareTag("Badge 1 W"))
+            {
+                WrongD1.SetActive(true);
+            }
+
+            if (col.gameObject.CompareTag("Badge 2 W"))
+            {
+                WrongD2.SetActive(true);
+            }
+        }
+    }
+
+    void OnTriggerExit(Collider col)
+    {
+        if (col.gameObject.CompareTag("Badge 3 R"))
+        {
+            RightD.SetActive(false);
+        }
+
+        if (col.gameObject.CompareTag("Badge 1 W"))
+        {
+            WrongD1.SetActive(false);
+        }
+
+        if (col.gameObject.CompareTag("Badge 2 W"))
+        {
+            WrongD2.SetActive(false);
+        }
+    }
+}
