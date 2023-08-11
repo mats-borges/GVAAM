ce85b4824af922d990d69a5e7b0eb200d3ff61a1
Jessica Fu <53541136+jjessicaf@users.noreply.github.com>
2023-08-10 21:59:22 -0700

    Revert "restore mode 3"
    This reverts commit a6c640eb2a10f19b90884b0a0834d6665b47e873, reversing
    changes made to 872abbe749a36fc116d0fddbf9ba39a66bfee42e.

--- Assets/Scripts/PagesideTextManager.cs
+++ Assets/Scripts/PagesideTextManager.cs
@@ -23,8 +23,6 @@ public class PagesideTextManager : MonoBehaviour
     [SerializeField] private GameObject bookManager;
     [SerializeField] private GameObject pageMarker;
     [SerializeField] private GameObject controlPanel;
-    [SerializeField] private GameObject leftQuad;
-    [SerializeField] private GameObject rightQuad;
     private GameObject folioMenuText;
 
     private List<string> LanguageTexts;
@@ -126,15 +124,12 @@ public class PagesideTextManager : MonoBehaviour
         {
             leftTextField.GetComponent<TextMeshPro>().text = splitArray[langNum][pageNum];
             rightTextField.GetComponent<TextMeshPro>().text = splitArray[langNum][pageNum + 1];
-            leftQuad.SetActive(true);
-            rightQuad.SetActive(true);
+            
         }
         else
         {
             leftTextField.GetComponent<TextMeshPro>().text = " ";
             rightTextField.GetComponent<TextMeshPro>().text = " ";
-            leftQuad.SetActive(false);
-            rightQuad.SetActive(false);
         }
     }
 
