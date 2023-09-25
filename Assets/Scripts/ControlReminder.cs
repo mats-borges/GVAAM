818dd723aed2699575b5f933550b6c2100f43ec3
Jessica Fu <53541136+jjessicaf@users.noreply.github.com>
2023-09-25 00:09:17 -0700

    updating pageside

--- Assets/Scripts/ControlReminder.cs
+++ Assets/Scripts/ControlReminder.cs
@@ -9,11 +9,14 @@ public class ControlReminder : MonoBehaviour
 
     [SerializeField] Material triggerMat;
     [SerializeField] private GameObject page;
+    private bool handViewed;
+    private bool contViewed;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        handViewed = false;
+        contViewed = false;
     }
 
     // Update is called once per frame
@@ -37,12 +40,13 @@ public class ControlReminder : MonoBehaviour
         string name = page.GetComponent<MeshRenderer>().material.name;
         Debug.Log(name);
         Debug.Log(triggerMat.name + " (Instance)");
-        if ((name == triggerMat.name + " (Instance)"))
+        if ((name == triggerMat.name + " (Instance)") && !handViewed)
         {
             HandDescription.SetActive(true);
         }
         else
         {
+            handViewed = true;
             HandDescription.SetActive(false);
         }
     }
@@ -52,12 +56,13 @@ public class ControlReminder : MonoBehaviour
         string name = page.GetComponent<MeshRenderer>().material.name;
         Debug.Log(name);
         Debug.Log(triggerMat.name + " (Instance)");
-        if ((name == triggerMat.name + " (Instance)"))
+        if ((name == triggerMat.name + " (Instance)") && !contViewed)
         {
             ControllerDescription.SetActive(true);
         }
         else
         {
+            contViewed = true;
             ControllerDescription.SetActive(false);
         }
     }
